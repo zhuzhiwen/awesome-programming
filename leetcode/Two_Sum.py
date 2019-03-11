6745c316ebbb57cd2dc9e6154b40eeb990eade47
zhuzhiwen2017 <zhuzhiwen2017@163.com>
2019-03-11 23:50:48 +0800

    leetcode:Two_Sum.py

---
+++ leetcode/Two_Sum.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return None
+            
+        d = {}
+        for i, item in enumerate(nums):
+            tmp  = target - item
+            
+            for key, value in d.items():
+                if value == tmp:
+                    return [key, i]
+            
+            d[i] = item
+            
+        return None
+
+l = [2, 7, 11, 15]
+s = Solution().twoSum(l, 18)
+print(s)
